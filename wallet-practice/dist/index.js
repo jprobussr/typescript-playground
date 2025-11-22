c0475ec5e5641fff9a0415f1bfab8f515a484115
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2025-11-22 09:43:32 -0600

    Initial Setup Currency Conversion Project --TS

---
+++ wallet-practice/dist/index.js
@@ -0,0 +1,51 @@
+"use strict";
+const conversionRates = {
+    euro: 1.1,
+    inr: 0.012,
+    usd: 1,
+};
+class Wallet {
+    constructor(currency, remaining) {
+        this.currency = currency;
+        this.stored = remaining;
+    }
+    spend(amount) {
+        if (this.stored < amount) {
+            return false;
+        }
+        this.stored -= amount;
+        return true;
+    }
+    transferTo(newCurrency) {
+        const newStored = (this.stored / conversionRates.usd) *
+            conversionRates[newCurrency];
+        this.stored = 0;
+        return new Wallet(newCurrency, newStored);
+    }
+}
+const purchaseInCurrency = (wallet, tag) => {
+    return wallet.spend(tag.price) && tag.item;
+};
+const americanWallet = new Wallet('usd', 50);
+const hat = purchaseInCurrency(americanWallet, {
+    currency: 'usd',
+    item: 'cowboy hat',
+    price: 34.99,
+});
+const falafel = purchaseInCurrency(americanWallet.transferTo('inr'), {
+    currency: 'euro',
+    item: 'falafel',
+    price: 10,
+});
+if (hat) {
+    console.log('I purchased a hat! 🤠');
+}
+else {
+    console.log("I couldn't afford the hat...");
+}
+if (falafel) {
+    console.log('I purchased falafel! 🥙');
+}
+else {
+    console.log("I couldn't afford the falafel...");
+}
---
+++ wallet-practice/index.ts
@@ -0,0 +1,69 @@
+const conversionRates = {
+  euro: 1.1,
+  inr: 0.012,
+  usd: 1,
+};
+
+class Wallet {
+  readonly currency: string;
+  private stored: number;
+
+  constructor(currency: string, remaining: number) {
+    this.currency = currency;
+    this.stored = remaining;
+  }
+
+  spend(amount: number) {
+    if (this.stored < amount) {
+      return false;
+    }
+    this.stored -= amount;
+    return true;
+  }
+
+  transferTo(newCurrency: string) {
+    const newStored =
+      (this.stored / conversionRates.usd) *
+      conversionRates[newCurrency as keyof typeof conversionRates];
+
+    this.stored = 0;
+
+    return new Wallet(newCurrency, newStored);
+  }
+}
+
+interface PriceTag {
+  currency: string;
+  item: string;
+  price: number;
+}
+
+const purchaseInCurrency = (wallet: Wallet, tag: PriceTag) => {
+  return wallet.spend(tag.price) && tag.item;
+};
+
+const americanWallet = new Wallet('usd', 50);
+
+const hat = purchaseInCurrency(americanWallet, {
+  currency: 'usd',
+  item: 'cowboy hat',
+  price: 34.99,
+});
+
+const falafel = purchaseInCurrency(americanWallet.transferTo('inr'), {
+  currency: 'euro',
+  item: 'falafel',
+  price: 10,
+});
+
+if (hat) {
+  console.log('I purchased a hat! 🤠');
+} else {
+  console.log("I couldn't afford the hat...");
+}
+
+if (falafel) {
+  console.log('I purchased falafel! 🥙');
+} else {
+  console.log("I couldn't afford the falafel...");
+}
