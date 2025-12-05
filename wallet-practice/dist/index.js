7e3505010d99eb39adcef00b778e6ea5ddf6020e
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2025-12-05 11:57:28 -0600

    Practice testing code

--- wallet-practice/dist/index.js
+++ wallet-practice/dist/index.js
@@ -17,8 +17,7 @@ class Wallet {
         return true;
     }
     transferTo(newCurrency) {
-        const newStored = (this.stored / conversionRates.usd) *
-            conversionRates[newCurrency];
+        const newStored = (this.stored / conversionRates.usd) * conversionRates[newCurrency];
         this.stored = 0;
         return new Wallet(newCurrency, newStored);
     }
@@ -26,14 +25,26 @@ class Wallet {
 const purchaseInCurrency = (wallet, tag) => {
     return wallet.spend(tag.price) && tag.item;
 };
-const americanWallet = new Wallet('usd', 500);
+const americanWallet = new Wallet('usd', 30);
 const hat = purchaseInCurrency(americanWallet, {
     currency: 'usd',
     item: 'cowboy hat',
     price: 34.99,
 });
-const falafel = purchaseInCurrency(americanWallet, {
+if (hat) {
+    console.log('I purchased a hat!');
+}
+else {
+    console.log('I did not get the hat today.');
+}
+const falafel = purchaseInCurrency(americanWallet.transferTo('euro'), {
     currency: 'euro',
     item: 'falafel',
     price: 10,
 });
+if (falafel) {
+    console.log('I just purchased a falafel.');
+}
+else {
+    console.log('I did not get the falafel today.');
+}
