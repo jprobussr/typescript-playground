2fbfd5f9bf7e6f13b4e23914c894441643f758b4
John Probus <johnprobussr@gmail.com>
2026-06-19 19:40:26 -0500

    Clean up TS playground

---
+++ dist/index.js
@@ -0,0 +1,23 @@
+"use strict";
+const products = [
+    {
+        name: "Wireless Mouse",
+        price: 29.99,
+        inStock: true,
+    },
+    {
+        name: "Gaming Keyboard",
+        price: 79.99,
+        inStock: false,
+    },
+    {
+        name: "USB-C Hub",
+        price: 49.99,
+        inStock: true,
+    },
+];
+products.forEach((product) => {
+    if (product.inStock) {
+        console.log(product.name);
+    }
+});
