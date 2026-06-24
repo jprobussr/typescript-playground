06d11427eddabc4c8e25782cb7c210323a8499f6
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2026-06-24 17:34:59 -0500

    Practice .some and .find TypeScript exercises

--- practice-data-ts/dist/index.js
+++ practice-data-ts/dist/index.js
@@ -1,39 +1,35 @@
 "use strict";
-const orders = [
+const movies = [
     {
-        item: 'Burger',
-        category: 'food',
-        price: 12.99,
-        quantity: 2,
-        isPaid: true,
+        title: 'The Dark Knight',
+        genre: 'action',
+        rating: 9.0,
+        runtime: 152,
+        isWatched: true,
     },
     {
-        item: 'Fries',
-        category: 'side',
-        price: 4.99,
-        quantity: 1,
-        isPaid: true,
+        title: 'Finding Nemo',
+        genre: 'animation',
+        rating: 8.2,
+        runtime: 100,
+        isWatched: true,
     },
     {
-        item: 'Lemonade',
-        category: 'drink',
-        price: 3.5,
-        quantity: 3,
-        isPaid: false,
+        title: 'Interstellar',
+        genre: 'sci-fi',
+        rating: 8.7,
+        runtime: 169,
+        isWatched: false,
     },
     {
-        item: 'Chicken Salad',
-        category: 'food',
-        price: 10.99,
-        quantity: 1,
-        isPaid: true,
+        title: 'The Grand Budapest Hotel',
+        genre: 'comedy',
+        rating: 8.1,
+        runtime: 99,
+        isWatched: false,
     },
 ];
-const sidesRevenue = orders.reduce((total, order) => {
-    if (order.category === 'side') {
-        return total + order.quantity * order.price;
-    }
-    ;
-    return total;
-}, 0);
-console.log(sidesRevenue);
+const horrorFilm = movies.find((movie) => {
+    return movie.genre === 'horror';
+});
+console.log(horrorFilm);
