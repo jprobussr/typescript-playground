8e281982db7086ecb05e71695692f07bd7c43953
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2026-06-29 15:46:00 -0500

    Practice restaurant filters and sorting

--- restaurant-orders/index.js
+++ restaurant-orders/index.js
@@ -1,10 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const orders_1 = require("./orders");
 const restaurants_1 = require("./restaurants");
-restaurants_1.restaurants.forEach((restaurant) => {
-    const distance = Number(restaurant.distance);
-    const priceLabel = restaurant.priceBracket === orders_1.PriceBracket.Low ? 'Affordable' : 'Premium';
-    const location = distance <= 5 ? 'Nearby' : 'Far Away';
-    console.log(`${restaurant.name} | ${priceLabel} | ${restaurant.deliveryTimeMinutes} min | ${location} | Open ${restaurant.openHour}:00-${restaurant.closeHour}:00`);
+const restaurantByOpenHour = [...restaurants_1.restaurants].sort((a, b) => {
+    return Number(a.openHour) - Number(b.openHour);
 });
+console.log(restaurantByOpenHour);
--- restaurant-orders/index.ts
+++ restaurant-orders/index.ts
@@ -2,17 +2,8 @@ import { orders, PriceBracket, type Order } from './orders';
 import { restaurants } from './restaurants';
 
 
-
-restaurants.forEach((restaurant) => {
-  const distance = Number(restaurant.distance);
-
-  const priceLabel =
-    restaurant.priceBracket === PriceBracket.Low ? 'Affordable' : 'Premium';
-
-  const location = distance <= 5 ? 'Nearby' : 'Far Away';
-
-  console.log(
-    `${restaurant.name} | ${priceLabel} | ${restaurant.deliveryTimeMinutes} min | ${location} | Open ${restaurant.openHour}:00-${restaurant.closeHour}:00`
-  );
+const restaurantByOpenHour = [...restaurants].sort((a, b) => {
+  return Number(a.openHour) - Number(b.openHour);
 });
 
+console.log(restaurantByOpenHour);
