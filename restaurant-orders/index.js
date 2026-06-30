14f35cee3cd3e0e8442d235388975dd0e22f8d9e
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2026-06-30 15:33:16 -0500

    Complete restaurant order filtering exercise

--- restaurant-orders/index.js
+++ restaurant-orders/index.js
@@ -1,7 +1,39 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const orders_1 = require("./orders");
 const restaurants_1 = require("./restaurants");
-const restaurantByOpenHour = [...restaurants_1.restaurants].sort((a, b) => {
-    return Number(a.openHour) - Number(b.openHour);
-});
-console.log(restaurantByOpenHour);
+const getMaxPrice = (price) => {
+    switch (price) {
+        case orders_1.PriceBracket.Low:
+            return 10;
+        case orders_1.PriceBracket.Medium:
+            return 20;
+        case orders_1.PriceBracket.High:
+            return 30;
+    }
+};
+// Get Orders
+const getOrders = (price, orders) => {
+    const maxPrice = getMaxPrice(price);
+    const filteredOrders = [];
+    orders.forEach((restaurantOrders) => {
+        const filteredRestaurantOrders = restaurantOrders.filter((order) => {
+            return order.price <= maxPrice;
+        });
+        filteredOrders.push(filteredRestaurantOrders);
+    });
+    return filteredOrders;
+};
+const printOrders = (restaurants, orders) => {
+    restaurants.forEach((restaurant, index) => {
+        const restaurantOrders = orders[index];
+        if (restaurantOrders.length > 0) {
+            console.log(restaurant.name);
+            restaurantOrders.forEach((order) => {
+                console.log(`- ${order.name}: $${order.price}`);
+            });
+        }
+    });
+};
+const eligibleOrders = getOrders(orders_1.PriceBracket.Low, orders_1.orders);
+printOrders(restaurants_1.restaurants, eligibleOrders);
--- restaurant-orders/index.ts
+++ restaurant-orders/index.ts
@@ -1,9 +1,48 @@
 import { orders, PriceBracket, type Order } from './orders';
-import { restaurants } from './restaurants';
+import { restaurants, type Restaurant } from './restaurants';
 
+const getMaxPrice = (price: PriceBracket): number => {
+  switch (price) {
+    case PriceBracket.Low:
+      return 10;
+    case PriceBracket.Medium:
+      return 20;
+    case PriceBracket.High:
+      return 30;
+  }
+};
 
-const restaurantByOpenHour = [...restaurants].sort((a, b) => {
-  return Number(a.openHour) - Number(b.openHour);
-});
+// Get Orders
 
-console.log(restaurantByOpenHour);
+const getOrders = (price: PriceBracket, orders: Order[][]): Order[][] => {
+  const maxPrice = getMaxPrice(price);
+  const filteredOrders: Order[][] = [];
+
+  orders.forEach((restaurantOrders) => {
+    const filteredRestaurantOrders = restaurantOrders.filter((order) => {
+      return order.price <= maxPrice;
+    });
+
+    filteredOrders.push(filteredRestaurantOrders);
+  });
+
+  return filteredOrders;
+};
+
+const printOrders = (restaurants: Restaurant[], orders: Order[][]): void => {
+  restaurants.forEach((restaurant, index) => {
+    const restaurantOrders = orders[index];
+
+    if (restaurantOrders.length > 0) {
+      console.log(restaurant.name);
+
+      restaurantOrders.forEach((order) => {
+        console.log(`- ${order.name}: $${order.price}`);
+      });
+    }
+  });
+};
+
+const eligibleOrders = getOrders(PriceBracket.Low, orders);
+
+printOrders(restaurants, eligibleOrders);
