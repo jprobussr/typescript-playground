8f4f91f9defd4c1438f1196230217a818d52bb00
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2026-07-04 15:32:30 -0500

    Practice with restaurant data and functions

--- restaurant-orders/index.js
+++ restaurant-orders/index.js
@@ -1,12 +1,41 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const orders_1 = require("./orders");
-const maxPrice = 20;
-const affordableOrders = [];
-orders_1.orders.forEach((restaurantOrders) => {
-    const affordableMeals = restaurantOrders.filter((order) => {
-        return order.price >= maxPrice;
+const restaurants_1 = require("./restaurants");
+const getMaxPrice = (price) => {
+    switch (price) {
+        case orders_1.PriceBracket.Low:
+            return 10;
+        case orders_1.PriceBracket.Medium:
+            return 20;
+        case orders_1.PriceBracket.High:
+            return 30;
+        default:
+            throw new Error('Invalid PriceBracket');
+    }
+};
+const getOrders = (price, orders) => {
+    const filteredOrders = [];
+    const maxPrice = getMaxPrice(price);
+    orders.forEach((restaurantOrders) => {
+        const filteredRestaurantOrders = restaurantOrders.filter((order) => {
+            return order.price <= maxPrice;
+        });
+        filteredOrders.push(filteredRestaurantOrders);
     });
-    affordableOrders.push(affordableMeals);
-});
-console.log(affordableOrders);
+    return filteredOrders;
+};
+const printOrders = (restaurants, orders) => {
+    restaurants.forEach((restaurant, index) => {
+        const restaurantOrders = orders[index];
+        if (restaurantOrders.length > 0) {
+            console.log(restaurant.name);
+            restaurantOrders.forEach((order) => {
+                console.log(`- ${order.name}: $${order.price}`);
+            });
+            console.log('');
+        }
+    });
+};
+const result = getOrders(orders_1.PriceBracket.Low, orders_1.orders);
+printOrders(restaurants_1.restaurants, result);
--- restaurant-orders/index.ts
+++ restaurant-orders/index.ts
@@ -1,18 +1,48 @@
 import { orders, PriceBracket, type Order } from './orders';
 import { restaurants, type Restaurant } from './restaurants';
 
+const getMaxPrice = (price: PriceBracket): number => {
+  switch (price) {
+    case PriceBracket.Low:
+      return 10;
+    case PriceBracket.Medium:
+      return 20;
+    case PriceBracket.High:
+      return 30;
+    default:
+      throw new Error('Invalid PriceBracket');
+  }
+};
 
+const getOrders = (price: PriceBracket, orders: Order[][]): Order[][] => {
+  const filteredOrders: Order[][] = [];
+  const maxPrice = getMaxPrice(price);
 
-const maxPrice = 20;
-const affordableOrders: Order[][] = [];
-
-orders.forEach((restaurantOrders) => {
-  const affordableMeals = restaurantOrders.filter((order) => {
-    return order.price >= maxPrice;
+  orders.forEach((restaurantOrders) => {
+    const filteredRestaurantOrders = restaurantOrders.filter((order) => {
+      return order.price <= maxPrice;
+    });
+    filteredOrders.push(filteredRestaurantOrders);
   });
-  affordableOrders.push(affordableMeals);
-});
+  return filteredOrders;
+};
 
-console.log(affordableOrders);
+const printOrders = (restaurants: Restaurant[], orders: Order[][]): void => {
+  restaurants.forEach((restaurant, index) => {
+    const restaurantOrders = orders[index];
 
+    if (restaurantOrders.length > 0) {
+      console.log(restaurant.name);
 
+      restaurantOrders.forEach((order) => {
+        console.log(`- ${order.name}: $${order.price}`);
+      });
+
+      console.log('');
+    }
+  });
+};
+
+const result = getOrders(PriceBracket.Low, orders);
+
+printOrders(restaurants, result);
