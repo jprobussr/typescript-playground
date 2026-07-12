236f9a13def02aa62a2eca601788ef8808f5e10b
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2026-07-12 12:33:40 -0500

    Practice TypeScript unions and event filtering

--- unionversity/index.js
+++ unionversity/index.js
@@ -1,7 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const courses_1 = require("./courses");
-const allCourses = courses_1.default.filter((course) => {
-    return course.eventType.includes('course');
-});
-console.log(allCourses);
+const studyGroups_1 = require("./studyGroups");
+console.log(studyGroups_1.default);
+const searchEvents = (options) => {
+    const events = options.eventType === 'courses' ? courses_1.default : studyGroups_1.default;
+    events.filter((event) => {
+        if (typeof options.query === 'number') {
+            return event.id === options.query;
+        }
+        if (typeof options.query === 'string') {
+            return event.keywords.includes(options.query);
+        }
+    });
+};
--- unionversity/index.ts
+++ unionversity/index.ts
@@ -1,8 +1,39 @@
-import courses from "./courses";
+import courses from './courses';
+import studyGroups from './studyGroups';
 
+console.log(studyGroups);
 
-const allCourses = courses.filter((course) => {
-    return course.eventType.includes('course');
-});
+type Course = {
+  id: number;
+  studyGroupId: number;
+  title: string;
+  keywords: string[];
+  eventType: string;
+};
 
-console.log(allCourses);
+type StudyGroup = {
+  id: number;
+  courseId: number;
+  title: string;
+  keywords: string[];
+  eventType: string;
+};
+
+type SearchEventsOptions = {
+  query: string | number;
+  eventType: 'courses' | 'groups';
+};
+
+const searchEvents = (options: SearchEventsOptions) => {
+  const events: Course[] | StudyGroup[] =
+    options.eventType === 'courses' ? courses : studyGroups;
+
+  events.filter((event: Course | StudyGroup) => {
+    if (typeof options.query === 'number') {
+        return event.id === options.query;
+    }
+    if (typeof options.query === 'string') {
+        return event.keywords.includes(options.query);
+    }
+  });
+};
