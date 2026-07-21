54a3db4f01977b05962b4d86c6259acf6301e6c8
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2026-07-21 13:44:56 -0500

    practice find and filter with volunteer data

---
+++ park-service-volunteer-program/index.js
@@ -0,0 +1,53 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const raccoon_meadows_log_1 = require("./raccoon-meadows-log");
+const volunteersWithLongActivities = raccoon_meadows_log_1.raccoonMeadowsVolunteers.filter((volunteer) => {
+    return volunteer.activities.some((activity) => {
+        return activity.hours > 3;
+    });
+});
+console.log(volunteersWithLongActivities);
+// const volunteersWithVerifiedActivities = raccoonMeadowsVolunteers.filter((volunteer) => {
+//     return volunteer.activities.some((activity) => {
+//         return activity.verified === 'Yes';
+//     });
+// });
+// console.log(volunteersWithVerifiedActivities);
+// const volunteersWithThreeHourActivity = raccoonMeadowsVolunteers.filter((volunteer) => {
+//     return volunteer.activities.some((activity) => {
+//         return activity.hours === 3;
+//     });
+// });
+// console.log(volunteersWithThreeHourActivity);
+// const volunteersWithIdAbove101 = raccoonMeadowsVolunteers.filter((volunteer) => {
+//     return volunteer.id > 101;
+// });
+// console.log(volunteersWithIdAbove101);
+// const volunteersWithMultipleActivities = raccoonMeadowsVolunteers.filter((volunteer) => {
+//     return volunteer.activities.length > 1;
+// });
+// console.log(volunteersWithMultipleActivities);
+// const volunteerWithUnverifiedActivity = raccoonMeadowsVolunteers.find((volunteer) => {
+//     return volunteer.activities.some((activity) => {
+//         return activity.verified === 'No';
+//     });
+// });
+// console.log(volunteerWithUnverifiedActivity);
+// const volunteerWithSixHourActivity = raccoonMeadowsVolunteers.find((volunteer) => {
+//     return volunteer.activities.some((activity) => {
+//         return  activity.hours === 6;
+//     });
+// });
+// console.log(volunteerWithSixHourActivity);
+// const volunteerWithThreeActivities = raccoonMeadowsVolunteers.find((volunteer) => {
+//     return volunteer.activities.length === 3;
+// });
+// console.log(volunteerWithThreeActivities);
+// const volunteerById = raccoonMeadowsVolunteers.find((volunteer) => {
+//     return volunteer.id === 102;
+// });
+// console.log(volunteerById);
+// const volunteerByName = raccoonMeadowsVolunteers.find((volunteer) => {
+//     return volunteer.name === 'Leigh Gilmour';
+// });
+// console.log(volunteerByName);
