c09f8ef9bd237b7ab62ab496c1b425a4402cc71a
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2026-08-11 14:13:46 -0500

    practice JavaScript interview challenges and reduce patterns

---
+++ interview-prep.ts/js-challenges/index.ts
@@ -0,0 +1,118 @@
+// const numbers = [12, 45, 7, 89, 34];
+
+// const smallestNumber = numbers.reduce((lowest, num) => {
+//     return num < lowest ? num : lowest;
+// }, numbers[0]);
+
+// console.log(smallestNumber);
+
+// const numbers = [12, 45, 7, 89, 34];
+
+// const largestNumber = numbers.reduce((highest, num) => {
+//     return highest < num ? num : highest;
+// }, numbers[0]);
+
+// console.log(largestNumber);
+
+// const word = 'frontend';
+
+// const reverseWord = word.split('').reverse().join('');
+
+// const isPalindrome = word === reverseWord;
+
+// console.log(isPalindrome);
+
+// const word = 'car';
+
+// const reverseWord = word.split('').reverse().join('');
+
+// const isPalindrome = word === reverseWord;
+
+// console.log(isPalindrome);
+
+// const word = 'racecar';
+
+// const reversedWord = word.split('').reverse().join('');
+
+// const isPalindrome = word === reversedWord;
+
+// console.log(isPalindrome);
+
+// const word = 'frontend developer';
+
+// const reverseWord = word.split('').reverse().join('');
+
+// console.log(reverseWord);
+
+// const word = 'developer';
+
+// const letters = word.split('');
+
+// const reverseLetters = letters.reverse();
+
+// const reverseWord = reverseLetters.join('');
+
+// console.log(reverseWord);
+
+// const word = 'frontend';
+// const letters = word.split('');
+
+// const reversedLetters = letters.reverse();
+
+// const reverseWord = reversedLetters.join('');
+
+// console.log(reverseWord);
+
+// for (let number = 1; number <= 100; number++) {
+//   if (number % 3 === 0 && number % 5 === 0) {
+//     console.log('FizzBuzz');
+//   } else if (number % 3 === 0) {
+//     console.log('Fizz');
+//   } else if (number % 5 === 0) {
+//     console.log('Buzz');
+//   } else {
+//     console.log(number);
+//   }
+// }
+
+// for (let number = 1; number <= 20; number++) {
+//     if (number % 3 === 0 && number % 5 === 0) {
+//         console.log('FizzBuzz');
+//     } else if (number % 3 === 0) {
+//         console.log('Fizz');
+//     } else if (number % 5 === 0) {
+//         console.log('Buzz');
+//     } else {
+//         console.log(number);
+//     }
+// };
+
+// const number = 15;
+
+// if (number % 3 === 0 && number % 5 === 0) {
+//     console.log('FizzBuzz');
+// } else if (number % 3 === 0) {
+//     console.log('Fizz');
+// } else if (number % 5 === 0) {
+//     console.log('Buzz');
+// } else {
+//     console.log(number);
+// }
+
+// const number = 15;
+
+// if (number % 3 === 0 && number % 5 === 0) {
+//     console.log('FizzBuzz');
+// }
+
+// const number = 10;
+
+// if (number % 5 === 0) {
+//     console.log('Buzz');
+// }
+
+// const number = 9;
+
+// if (number % 3 === 0) {
+//     console.log('Fizz');
+// }
