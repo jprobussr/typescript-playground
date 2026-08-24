6bec7ec7409073cc56c26e5c5dca82aba8463058
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2026-08-24 15:37:58 -0500

    practice TypeScript functions loops and conditionals

--- interview-prep.ts/js-challenges/practice.ts
+++ interview-prep.ts/js-challenges/practice.ts
@@ -1,34 +1,197 @@
-const developers = [
-  {
-    name: 'Maya',
-    experience: 4,
-    isAvailable: true,
-  },
-  {
-    name: 'Liam',
-    experience: 2,
-    isAvailable: false,
-  },
-  {
-    name: 'Sofia',
-    experience: 5,
-    isAvailable: true,
-  },
-];
+// const username = 'john_probus';
 
-const getAvailableDeveloperNames = () => {
-  let availableDevelopers = [];
+// const isValidUserName = (username: string): boolean => {
+//   return username.length >= 6 && username.includes('_');
+// };
 
-  for (let developer of developers) {
-    if (developer.isAvailable) {
-      availableDevelopers.push(developer.name);
-    }
-  }
+// console.log(isValidUserName(username));
 
-  return availableDevelopers;
-};
+// type User = {
+//   username: string;
+//   isAdmin: boolean;
+// };
 
-console.log(getAvailableDeveloperNames());
+// const users: User[] = [
+//   { username: 'maya', isAdmin: false },
+//   { username: 'liam', isAdmin: false },
+//   { username: 'sofia', isAdmin: true },
+//   { username: 'noah', isAdmin: false },
+// ];
+
+// const getAdminUserName = (users: User[]): string => {
+//   for (let user of users) {
+//     if (user.isAdmin) {
+//       return user.username;
+//     }
+//   }
+
+//   return `Admin not found.`;
+// }
+
+// console.log(getAdminUserName(users));
+
+// const numbers = [3, 8, 12, 5, 20, 7];
+
+// const countEvenNumbers = (number: number[]): number => {
+//   let count = 0;
+
+//   for (let number of numbers) {
+//     if (number % 2 === 0) {
+//       count += 1;
+//     }
+//   }
+
+//   return count;
+// };
+
+// console.log(countEvenNumbers(numbers));
+
+// type Employee = {
+//   name: string;
+//   hoursWorked: number;
+// };
+
+// const employee: Employee = {
+//   name: 'Maya',
+//   hoursWorked: 43,
+// };
+
+// const getWorkStatus = (employee: Employee): string => {
+//   if (employee.hoursWorked >= 40) {
+//     return `${employee.name} worked full-time this week.`;
+//   } else {
+//     return `${employee.name} worked part-time this week.`;
+//   }
+// };
+
+// console.log(getWorkStatus(employee));
+
+// const temperatures = [72, 88, 95, 67, 101, 84];
+
+// const getHotTemps = (temperatures: number[]): number[] => {
+//   const hotTemps: number[] = [];
+
+//   for (let temp of temperatures) {
+//     if (temp >= 90) {
+//       hotTemps.push(temp);
+//     }
+//   }
+
+//   return hotTemps;
+// };
+
+// console.log(getHotTemps(temperatures));
+
+// const email = '  JOHN.PROBUS@EXAMPLE.COM  ';
+
+// const normalizeEmail = (str: string): string => {
+//   return str.trim().toLowerCase();
+// }
+
+// console.log(normalizeEmail(email));
+
+// type Order = {
+//   id: number;
+//   status: 'pending' | 'shipped' | 'delivered';
+//   total: number;
+// };
+
+// const order: Order = {
+//   id: 101,
+//   status: 'delivered',
+//   total: 125,
+// };
+
+// const getOrderMessage = (order: Order): string => {
+//   if (order.status === 'pending') {
+//     return `Order ${order.id} is still pending.`;
+//   } else if (order.status === 'shipped') {
+//     return `Order ${order.id} has shipped.`;
+//   } else {
+//     return `Order ${order.id} was delivered.`;
+//   }
+// };
+
+// console.log(getOrderMessage(order));
+
+// type Student = {
+//   name: string;
+//   score: number;
+// };
+
+// const students: Student[] = [
+//   { name: 'Maya', score: 88 },
+//   { name: 'Liam', score: 67 },
+//   { name: 'Sofia', score: 94 },
+//   { name: 'Noah', score: 72 },
+// ];
+
+// const getPassingStudentNames = (students: Student[]): string[] => {
+//   const passingStudents: string[] = [];
+
+//   for (let student of students) {
+//     if (student.score >= 70) {
+//       passingStudents.push(student.name);
+//     }
+//   }
+//   return passingStudents;
+// };
+
+// console.log(getPassingStudentNames(students));
+
+// type Product = {
+//   name: string;
+//   price: number;
+//   inStock: boolean;
+// };
+
+// const product: Product = {
+//   name: 'Mechanical Keyboard',
+//   price: 89.99,
+//   inStock: false,
+// };
+
+// const getProductMessage = (product: Product): string => {
+//   if (product.inStock) {
+//     return `${product.name} costs $${product.price} and is in stock`;
+//   } else {
+//     return `${product.name} is currently unavailable.`;
+//   }
+// };
+
+// console.log(getProductMessage(product));
+
+// const developers = [
+//   {
+//     name: 'Maya',
+//     experience: 4,
+//     isAvailable: true,
+//   },
+//   {
+//     name: 'Liam',
+//     experience: 2,
+//     isAvailable: false,
+//   },
+//   {
+//     name: 'Sofia',
+//     experience: 5,
+//     isAvailable: true,
+//   },
+// ];
+
+// const getAvailableDeveloperNames = () => {
+//   let availableDevelopers = [];
+
+//   for (let developer of developers) {
+//     if (developer.isAvailable) {
+//       availableDevelopers.push(developer.name);
+//     }
+//   }
+
+//   return availableDevelopers;
+// };
+
+// console.log(getAvailableDeveloperNames());
 
 // for (let developer of developers) {
 //     if (developer.isAvailable) {
